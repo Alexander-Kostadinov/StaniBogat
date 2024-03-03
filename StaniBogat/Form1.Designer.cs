ed8787f43dc6c13e780c325c0f37194e3f45abf5
Aleksandar Kostadinov <alekkostadinov@tu-sofia.bg>
2024-03-03 13:05:07 +0200

    Add project files.

---
+++ StaniBogat/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace StaniBogat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Image = global::StaniBogat.Properties.Resources.start_button;
+            this.button1.Location = new System.Drawing.Point(334, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 85);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(842, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1           1 000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(842, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2           2 000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(841, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "3           5 000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(840, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "4         10 000";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(841, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "5         20 000";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(840, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "6         50 000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(840, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "7       100 000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(840, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "8       250 000";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(840, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "9       500 000";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(835, 83);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "10   1 000 000";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(783, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 47);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Take the money!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(855, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 50);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Bonus Time";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Paint += new System.Windows.Forms.PaintEventHandler(this.button3_Paint);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(776, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 50);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "50/50";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Paint += new System.Windows.Forms.PaintEventHandler(this.button4_Paint);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(12, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 47);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Best Players";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(301, 348);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(498, 348);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(301, 404);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(498, 404);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(940, 451);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ StaniBogat/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Forms.Integration;
+
+namespace StaniBogat
+{
+    public partial class Form1 : Form
+    {
+        public int X;
+        public int Y;
+        public int Time;
+        public int Count;
+        public bool IsStarted;
+        public bool IsUsedBtn1;
+        public bool IsUsedBtn2;
+        public string TimeText;
+        public Bitmap BackScene;
+        public bool IsTimeRunnig;
+        public ElementHost Element;
+        public TextBlock AnimationText;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Count = 1;
+            Time = 120;
+            IsStarted = false;
+            TimeText = "02:00";
+            IsUsedBtn1 = false;
+            IsUsedBtn2 = false;
+            IsTimeRunnig = false;
+            timer1.Interval = 1000;
+            Element = new ElementHost();
+            AnimationText = new TextBlock();
+
+            var directory = Path.GetDirectoryName(Environment.CurrentDirectory);
+            var path = directory.Replace("bin", "") + "Studio.bmp";
+            BackScene = new Bitmap(path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = (int)(Screen.PrimaryScreen.Bounds.Width / 1.25);
+            this.Height = (int)(Screen.PrimaryScreen.Bounds.Height / 1.25);
+
+            for (int i = 0; i < Controls.Count; i++)
+            {
+                Controls[i].Visible = false;
+            }
+
+            //ElementHost elementHost = new ElementHost();
+            //elementHost.Width = this.Width;
+            //elementHost.Height = this.Height;
+            //elementHost.Location = new System.Drawing.Point(0, 0);
+
+            //var directory = Path.GetDirectoryName(Environment.CurrentDirectory);
+            //var path = directory.Replace("bin", "") + "logo.gif";
+            //MediaElement gif = new MediaElement();
+            //gif.LoadedBehavior = MediaState.Manual;
+            //gif.UnloadedBehavior = MediaState.Manual;
+
+            //if (File.Exists(path))
+            //{
+            //    gif.Source = new Uri(path);
+            //    elementHost.Child = gif;
+            //    Controls.Add(elementHost);
+            //    gif.Play();
+
+            //    gif.MediaEnded += (s, ev) =>
+            //    {
+            //        gif.Close();
+            //        gif = null;
+            //        elementHost.Dispose();
+            //        Controls.Remove(elementHost);
+            //        elementHost = null;
+            //    };
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Time--;
+
+            if (Time <= 0)
+            {
+                timer1.Stop();
+            }
+
+            if (Time == 180)
+            {
+                TimeText = "03:00";
+            }
+            else if (Time >= 120 && Time < 180)
+            {
+                if (Time < 130)
+                {
+                    TimeText = "02:" + '0' + (Time - 120);
+                }
+                else
+                {
+                    TimeText = "02:" + (Time - 120);
+                }
+            }
+            else if (Time < 120 && Time >= 60)
+            {
+                if (Time < 70)
+                {
+                    TimeText = "01:" + '0' + (Time - 60);
+                }
+                else
+                {
+                    TimeText = "01:" + (Time - 60);
+                }
+            }
+            else if (Time < 60)
+            {
+                if (Time < 10)
+                {
+                    TimeText = "00:" + '0' + Time;
+                }
+                else
+                {
+                    TimeText = "00:" + Time;
+                }
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (IsStarted == false)
+            {
+                button1.Visible = true;
+
+                //var animation = new DoubleAnimation
+                //{
+                //    From = 0.0,
+                //    To = 1.0,
+                //    Duration = TimeSpan.FromSeconds(2),
+                //    AutoReverse = true,
+                //    RepeatBehavior = RepeatBehavior.Forever,
+                //};
+
+                //Element.Dock = DockStyle.Top;
+                //Element.Child = AnimationText;
+                //Element.Width = this.Width / 2;
+                //Element.Height = this.Height / 2;
+
+                //AnimationText.FontSize = 42;
+                //AnimationText.FontStyle = FontStyles.Italic;
+                //AnimationText.TextAlignment = TextAlignment.Center;
+                //AnimationText.VerticalAlignment = VerticalAlignment.Center;
+                //AnimationText.Text = "Press the button to start the game !";
+                //AnimationText.Foreground = System.Windows.Media.Brushes.LightGoldenrodYellow;
+                //AnimationText.BeginAnimation(UIElement.OpacityProperty, animation);
+
+                //button1.Location = new System.Drawing.Point((Width -
+                //    button1.Width) / 2, (Height - button1.Height) / 2);
+
+                //Controls.Add(Element);
+            }
+            if (IsStarted == true)
+            {
+                Pen pen = new Pen(Color.DarkBlue, 8);
+                Brush brush = new SolidBrush(Color.Black);
+
+                e.Graphics.DrawImage(BackScene, 0, 0, Width, Height);
+                e.Graphics.DrawRectangle(pen, 10, 10, button5.Width, button5.Height);
+                e.Graphics.DrawRectangle(pen, label10.Location.X 
+                    - 20, 4, Width - label10.Location.X - 2, Height - 55);
+                e.Graphics.FillRectangle(brush, label10.Location.X 
+                    - 20, 4, Width - label10.Location.X - 2, Height - 55);
+                e.Graphics.DrawEllipse(pen, ((label10.Location.X - 20)
+                    / 2) - (TimeText.Length * 30) / 2 - 25, 10, 200, 100);
+                e.Graphics.FillEllipse(brush, ((label10.Location.X - 20) 
+                    / 2) - (TimeText.Length * 30) / 2 - 25, 10, 200, 100);
+                brush = new SolidBrush(Color.White);
+                e.Graphics.DrawString(TimeText, new Font("Arial", 32),
+                    brush, new PointF(((label10.Location.X - 20) / 2) - (TimeText.Length * 30) / 2, 25));
+                brush = new SolidBrush(Color.DarkGoldenrod);
+                e.Graphics.FillRectangle(brush, X - 10, Y, label10.Width + 5, label10.Height);
+
+                pen.Dispose();
+                brush.Dispose();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            IsStarted = true;
+            Element.Dispose();
+            Controls.Remove(Element);
+            Controls.Remove(button1);
+            Y = 125;
+
+            for (int i = 0; i < Controls.Count; i++)
+            {
+                if (Controls[i].GetType() == typeof(System.Windows.Forms.Label))
+                {
+                    Controls[i].ForeColor = Color.White;
+                    Controls[i].Font = new Font("Arial", 24);
+                    Controls[i].BackColor = Color.Transparent;
+                    Controls[i].Location = new System.Drawing.Point(
+                        Width - (Controls[i].Width + 50), Y);
+                    Y += Controls[i].Height + 15;
+                }
+
+                Controls[i].Visible = true;
+            }
+
+            X = label1.Location.X;
+            Y = label1.Location.Y;
+            label1.ForeColor = Color.Black;
+
+            button4.ForeColor = Color.White;
+            button4.BackColor = Color.DarkBlue;
+            button4.Font = new Font("Arial", 20);
+            button4.Location = new System.Drawing.Point(label10.Location.X + 10, 20);
+
+            button3.Text = "Bonus\nTime";
+            button3.ForeColor = Color.White;
+            button3.BackColor = Color.DarkBlue;
+            button3.Font = new Font("Arial", 20);
+            button3.Location = new System.Drawing.Point(
+                button4.Location.X + button4.Width + 25, 20);
+            button4.Size = button3.Size;
+
+            button2.ForeColor = Color.Black;
+            button2.Font = new Font("Italic", 20);
+            button2.BackColor = Color.DarkGoldenrod;
+            button2.Height += 10;
+            button2.Location = new System.Drawing.Point(X - 10, Y + 30 + label1.Height);
+
+            button5.Text = "Best\nplayers";
+            button5.ForeColor = Color.White;
+            button5.BackColor = Color.Black;
+            button5.Font = new Font("Arial", 16);
+            button5.Width += 5;
+            button5.Height += 15;
+            button5.Location = new System.Drawing.Point(10, 10);
+
+            Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            System.Windows.MessageBox.Show("Congratulations!\nYour game finished!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (IsUsedBtn2 == false)
+            {
+                IsUsedBtn2 = true;
+                Time += 60;
+                Refresh();
+            }
+        }
+
+        private void button3_Paint(object sender, PaintEventArgs e)
+        {
+            if (IsUsedBtn2)
+            {
+                Pen pen = new Pen(Color.Red, 8);
+                e.Graphics.DrawLine(pen, 0, button3.Height, button3.Width, 0);
+                e.Graphics.DrawLine(pen, 0, 0, button3.Width, button3.Height);
+            }
+        }
+
+        private void button4_Paint(object sender, PaintEventArgs e)
+        {
+            if (IsUsedBtn1)
+            {
+                Pen pen = new Pen(Color.Red, 8);
+                e.Graphics.DrawLine(pen, 0, button4.Height, button4.Width, 0);
+                e.Graphics.DrawLine(pen, 0, 0, button4.Width, button4.Height);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (IsUsedBtn1 == false)
+            {
+                IsUsedBtn1 = true;
+                Refresh();
+            }
+        }
+    }
+}
